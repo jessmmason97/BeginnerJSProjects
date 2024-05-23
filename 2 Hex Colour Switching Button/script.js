acd40e606bee64ee77f3a0eedcac8ddcd75af730
Jess Mason <jessmmason97@gmail.com>
2024-05-23 18:26:30 +0100

    message

--- 2 Hex Colour Switching Button/script.js
+++ 2 Hex Colour Switching Button/script.js
@@ -1,12 +1,13 @@
+const hexCharacters = ["0","1","2","3","4","5","6","7","8","9","A", "B", "C", "D", "E", "F"];
+
 function changeColour()
 {
-  var hexNumbers = ["0","1","2","3","4","5","6","7","8","9","A", "B", "C", "D", "E", "F"];
-
   var hexcode = '';
   
+  // generate a random 5 character hexcode using the characters from the hexCharacters array
   for(var i = 0; i<6; i++){
-    var randomIndex = Math.floor(Math.random() * hexNumbers.length);
-    hexcode += hexNumbers[randomIndex];
+    var randomIndex = Math.floor(Math.random() * hexCharacters.length);
+    hexcode += hexCharacters[randomIndex];
   }
 
   document.getElementById("hexCode").innerHTML = hexcode;
